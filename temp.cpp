475c139dc530de99057f1bbeb26f9bfc42b1114c
orzzzl <zhuangzeleng19920731@gmail.com>
2015-11-27 13:02:06 -0500

    template

---
+++ temp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define vi vector<int>
+#define PB push_back
+#define MP make_pair
+#define S(xs) scanf("%d",&xs)
+#define SS(xx,yy) scanf("%d%d",&xx,&yy)
+#define P(xss) printf("%d\n",xss);
+#define eps 1e-8
+#define PI 3.141592653589793
+#define MM 1000000007
+
+int main () {
+
+    return 0;
+}
