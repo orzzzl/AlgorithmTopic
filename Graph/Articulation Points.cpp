7eebcf63ad25808cf145287b65deb53c3275c69b
orzzzl <zhuangzeleng19920731@gmail.com>
2016-04-07 11:33:42 +0800

    EMPTY

--- Graph/Articulation Points.cpp
+++ Graph/Articulation Points.cpp
@@ -1,14 +1,9 @@
-// 0-based
-// A class that represents an undirected graph
 class Graph {
-    int V;    // No. of vertices
-    list<int> *adj;
     void APUtil(int u, bool visited[], int disc[], int low[], int parent[], set<int> &ap) {
-        static int time = 0;
         int children = 0;
         visited[u] = true;
         disc[u] = low[u] = ++time;
-        list<int>::iterator i;
+        set<int>::iterator i;
         for (i = adj[u].begin(); i != adj[u].end(); ++i) {
             int v = *i;
             if (!visited[v]) {
@@ -18,26 +13,32 @@ class Graph {
                 low[u]  = min(low[u], low[v]);
                 if (parent[u] == -1 && children > 1)    ap.insert(u);
                 if (parent[u] != -1 && low[v] >= disc[u])    ap.insert(u);
+                if (low[v] > disc[u]) bridges.insert(make_pair(min(u, v), max(u, v)));
             } else if (v != parent[u]) {
                 low[u]  = min(low[u], disc[v]);
             }
         }
     }
 public:
+    int V;    // No. of vertices
+    set<int> *adj;
+    int time;
+    set<pair<int, int> > bridges;
     Graph(int V) {
         this->V = V;
-        adj = new list<int>[V];
+        adj = new set<int>[V];
+        time = 0;
     }
     void addEdge(int v, int w) {
-        adj[v].push_back(w);
-        adj[w].push_back(v);  // Note: the graph is undirected
+        adj[v].insert(w);
+        adj[w].insert(v);  // Note: the graph is undirected
     }
     set<int> AP() {
         bool *visited = new bool[V];
         int *disc = new int[V];
         int *low = new int[V];
         int *parent = new int[V];
-        set<int> ap; 
+        set<int> ap;
         for (int i = 0; i < V; i++) {
             parent[i] = -1;
             visited[i] = false;
